9e6c531ac97ca19856b46e181d2b2f1d7a3b1c2b
Guido Vezzoni <guido.vezzoni.development@gmail.com>
2022-02-19 22:50:21 +0100

    ViewModel tested

--- app/src/test/java/com/guidovezzoni/bingeworthyshow2/presentation/tvshowlist/model/UiModelsMother.kt
+++ app/src/test/java/com/guidovezzoni/bingeworthyshow2/presentation/tvshowlist/model/UiModelsMother.kt
@@ -1,13 +1,11 @@
 package com.guidovezzoni.bingeworthyshow2.presentation.tvshowlist.model
 
-import com.guidovezzoni.bingeworthyshow2.domain.model.PaginatedListDomainModel
-
 @Suppress("MemberVisibilityCanBePrivate")
 object UiModelsMother {
-    val TV_SHOW_UI_MODEL_1 = TvShowUiModel(0, "Lucifer", "34tbjhb", 10.0)
-    val TV_SHOW_UI_MODEL_2 = TvShowUiModel(1, "Lupin", "ete54fds", 10.0)
+    val TV_SHOW_UI_MODEL_1 = TvShowUiModel(0, "Lucifer", "https://base.urlw78034tbjhb", 10.0)
+    val TV_SHOW_UI_MODEL_2 = TvShowUiModel(1, "Lupin", "https://base.urlw780ete54fds", 10.0)
 
-    val TV_SHOW_UI_MODEL_PAGINATED_LIST = PaginatedListDomainModel(
+    val TV_SHOW_UI_MODEL_PAGINATED_LIST = PaginatedListUiModel(
         items = listOf(TV_SHOW_UI_MODEL_1, TV_SHOW_UI_MODEL_2),
         page = 1,
         totalItems = 2,
